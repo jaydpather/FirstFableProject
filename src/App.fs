d56c70089c634280b8802e5ea2d88e11de38b30d
Jayd <jaydpather@gmail.com>
2020-01-28 15:52:04 +0100

    adding alert message box

--- src/App.fs
+++ src/App.fs
@@ -1,11 +1,19 @@
-module App
+module AppOld
 
+open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Fable.Core.Util
+
+[<Emit("alert('$0')")>]
+let alert msg = jsNative
 
 let window = Browser.Dom.window
 let myParagraph:Browser.Types.HTMLParagraphElement = unbox window.document.getElementById "myParagraph"
 
+
 myParagraph.className <- "error"
+
+alert "test msg"
 
 printfn "page loaded"
